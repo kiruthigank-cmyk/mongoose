d01d8992b25262371733c021f220e72f81a8b22d
kiruthigank-cmyk <kiruthigauthayakumar@gmail.com>
2026-01-31 13:41:26 +0530

    demo

---
+++ expdemo5.js
@@ -0,0 +1,21 @@
+const exp=require("express")
+const app=exp()
+const mongoose=require("mongoose")
+const User=require("./userModel")     //import
+
+mongoose.connect("mongodb://127.0.0.1:27017/mykiru").then(()=>{
+    console.log("connection success using mongodb lib")
+}).catch(()=>{
+    console.log("Error connection")
+})
+async function dis()
+{
+    try{
+    const result=await User.exists({sname:"kiruthi"})
+    console.log(result)
+    }
+    catch(e){
+        console.log(message)
+    }
+}
+dis()
